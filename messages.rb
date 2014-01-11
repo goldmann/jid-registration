37330b1be2a75ed99513a41e2da2b12738f54dac
Marek Goldmann <marek.goldmann@gmail.com>
2014-01-11 20:42:02 +0100

    Use messages from messages.rb

---
+++ messages.rb
@@ -0,0 +1,21 @@
+class Messages
+  def self.errors
+    {
+      :invalid_captcha => { :log => "ERR-001: Invalid captcha", :message => "Niepoprawny tekst z obrazka, spróbuj jeszcze raz"},
+      :empty_password => { :log => "ERR-002: Empty password", :message => "Nie podano hasła"},
+      :password_too_short => { :log => "ERR-003: Password too short", :message => "Hasło jest zbyt krótkie, użyj minimum 6 znaków"},
+      :password_mismatch => { :log => "ERR-004: Password do not match", :message => "Podane hasła nie zgadzają się"},
+      :user_exists => { :log => "ERR-005: User already exists", :message => "Użytkownik '#USERNAME#' już istnieje, wybierz inną nazwę"},
+      :request_not_authorized => { :log => "ERR-006: Request is not authorized", :message => "Wystąpił błąd podczas zakładania konta - w przypadku, gdy sytuacja będzie się powtarzać - skontaktuj się z administratorem"},
+      :shared_group_exception => { :log => "ERR-007: Shared group exception", :message => "Wystąpił błąd podczas zakładania konta - w przypadku, gdy sytuacja będzie się powtarzać - skontaktuj się z administratorem"},
+      :registration_disabled => { :log => "ERR-008: Registration is disabled", :message => "Rejestracja jest wyłączona - spróbuj w innym momencie"},
+      :unknown_error => { :log => "ERR-009: Bad thing happened", :message => "Wystąpił błąd podczas zakładania konta - w przypadku, gdy sytuacja będzie się powtarzać - skontaktuj się z administratorem"}
+    }
+  end
+
+  def self.infos
+    {
+      :user_registered => { :log => "OOK-001: Bad thing happened", :message => "Konto '#USERNAME#' zostało założone"}
+    }
+  end
+end
--- register.rb
+++ register.rb
@@ -6,15 +6,22 @@ require 'haml'
 require 'rack/recaptcha'
 require 'rest_client'
 
+require './messages'
+
 configure do
   set :server, 'thin'
-  # set :environment, :production
   set :public_folder, File.dirname(__FILE__) + '/public'
-  set :session_secret, settings.session_secret
   set :protection, :origin_whitelist => ['https://secure.jid.pl']
 
   config_file 'config.yml'
 
+  abort "No session secret provided" if settings.session_secret.nil?
+  abort "No recaptcha public key provided" if settings.recaptcha['public_key'].nil?
+  abort "No recaptcha private key provided" if settings.recaptcha['private_key'].nil?
+  abort "No URL provided" if settings.url.nil?
+  abort "No OpenFire secret provided" if settings.openfire_secret.nil?
+
+  set :session_secret, settings.session_secret
   use Rack::Recaptcha, :public_key => settings.recaptcha['public_key'], :private_key => settings.recaptcha['private_key']
 
   enable :sessions
@@ -39,8 +46,8 @@ def show_message(msg, options = {})
     :redirect => true
   }.merge(options)
 
-  logger.send(options[:type], "#{msg['log']}, from IP: #{request.ip}")
-  flash[options[:type]] = eval("\"" + msg['message'] + "\"")
+  logger.send(options[:type], "#{msg[:log]}, from IP: #{request.ip}")
+  flash[options[:type]] = eval("\"" + msg[:message] + "\"")
   redirect "/" if options[:redirect]
 end
 
@@ -49,10 +56,10 @@ get '/' do
 end
 
 post '/register' do
-  show_message(settings.messages['invalid_captcha']) unless recaptcha_valid?
-  show_message(settings.messages['empty_password']) if params[:password].length == 0
-  show_message(settings.messages['password_too_short']) if params[:password].length < 6
-  show_message(settings.messages['password_mismatch']) unless params[:password].eql?(params[:passwordrepeat])
+  show_message(Messages.errors[:invalid_captcha]) unless recaptcha_valid?
+  show_message(Messages.errors[:empty_password]) if params[:password].length == 0
+  show_message(Messages.errors[:password_too_short]) if params[:password].length < 6
+  show_message(Messages.errors[:password_mismatch]) unless params[:password].eql?(params[:passwordrepeat])
 
   logger.info "Registering new user with username '#{params['username']}'..."
 
@@ -67,17 +74,17 @@ post '/register' do
     }
   rescue => e
     logger.error e.backtrace.join("\n")
-    show_message(settings.messages['unknown_error'])
+    show_message(Messages.errors[:unknown_error])
   end
 
   logger.debug "RAW: #{result.strip}"
 
-  show_message(settings.messages['user_exists']) if result.include?('UserAlreadyExistsException')
-  show_message(settings.messages['request_not_authorized']) if result.include?('RequestNotAuthorised')
-  show_message(settings.messages['shared_group_exception']) if result.include?('SharedGroupException')
-  show_message(settings.messages['registration_disabled']) if result.include?('UserServiceDisabled')
-  show_message(settings.messages['user_registered'], :type => :info) if result.downcase.include?('<result>ok</result>')
-  show_message(settings.messages['unknown_error'])
+  show_message(Messages.errors[:user_exists]) if result.include?('UserAlreadyExistsException')
+  show_message(Messages.errors[:request_not_authorized]) if result.include?('RequestNotAuthorised')
+  show_message(Messages.errors[:shared_group_exception]) if result.include?('SharedGroupException')
+  show_message(Messages.errors[:registration_disabled]) if result.include?('UserServiceDisabled')
+  show_message(Messages.infos[:user_registered], :type => :info) if result.downcase.include?('<result>ok</result>')
+  show_message(Messages.errors[:unknown_error])
 end
 
 not_found do
